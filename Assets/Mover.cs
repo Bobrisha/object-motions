78c58a54a95ffbe97e2c303668d5974e8ead2c68
Konstantin Sarambaev <konstantin.s@playgendary.com>
2019-09-03 19:56:17 +0300

    - improve spiral logic

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -34,14 +34,10 @@ public class Mover : MonoBehaviour
     // f = (0, 2pi)
     // a = spiral steps
 
-    float f = 0;
-    float a = 0.1f;
-
+  
     void Update()
     {
-        float p = a * f / 2 / Mathf.PI;
-        objectToMove.position = new Vector2(p * Mathf.Cos(f), p * Mathf.Sin(f));
-        f+= 0.1f;
+
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -49,23 +45,34 @@ public class Mover : MonoBehaviour
             startTime = Time.time;
             startPosition = objectToMove.transform.position;
 
-            switch (trajectory)
-            {
-                case Trajectories.Linear:
-                    distance = Vector2.Distance(startPosition, clickPosition);
-                    break;
+            //switch (trajectory)
+            //{
+                 distance = Vector2.Distance(startPosition, clickPosition);
+                //case Trajectories.Linear:
+                   
+                //    break;
 
-                case Trajectories.Spikes:
-                    distance = Mathf.Sqrt(Mathf.Pow((Vector2.Distance(startPosition, clickPosition) / spikesCount), 2) + spikesHeight * spikesHeight) * spikesCount * 2;
-                    break;
-            }
+                //case Trajectories.Spikes:
+                    //distance = Mathf.Sqrt(Mathf.Pow((Vector2.Distance(startPosition, clickPosition) / spikesCount), 2) + spikesHeight * spikesHeight) * spikesCount * 2;
+                    //break;
+            //}
 
             if (MoveCoroutine != null)
             {
                 StopCoroutine(MoveCoroutine);
             }
 
-            MoveCoroutine = StartCoroutine(Move());
+            switch (trajectory)
+            {
+                case Trajectories.Linear:
+                    MoveCoroutine = StartCoroutine(Move());
+                    break;
+
+                case Trajectories.Spiral:
+                    MoveCoroutine = StartCoroutine(SpiralMove());
+                    break;
+            }
+
         }
     }
 
@@ -82,7 +89,46 @@ public class Mover : MonoBehaviour
                     float fractionDitance = (Time.time - startTime) / time;
                     objectToMove.position = Vector3.Lerp(startPosition, clickPosition, fractionDitance);
                     break;
+
             }
+        }
+    }
+
+
+    float angleStep;
+    float startAngle;
+
+    float angle;
+
+    float a = 1;
+
+
+    IEnumerator SpiralMove()
+    {
+        Vector2 offset = (Vector2)objectToMove.position - clickPosition;
+        startAngle = Mathf.Atan2(offset.y , offset.x);
+
+        //if (startAngle < 0f)
+        //{
+        //    startAngle = 2 * Mathf.PI + startAngle;
+        //}
+
+        print(startAngle);
+        angle = startAngle + Mathf.Floor(distance / a) * 2 * Mathf.PI;
+
+        while (angle > 0.0f)
+        {
+            yield return null;
+
+            //angleStep = Time.deltaTime * startAngle / time;
+            //angle -= angleStep;
+            angle -= 0.1f;
+            //print(angle);
+
+            float p = a * angle / 2.0f / Mathf.PI;
+            objectToMove.position = new Vector2(p * Mathf.Cos(angle), p * Mathf.Sin(angle)) + clickPosition;
+
+          
         }
     }
 }
